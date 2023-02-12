c87b44e2a009bca76b17587c7d7f6b5b6f32e624
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-12 18:08:45 +0400

    Update Animal.java

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -6,6 +6,9 @@ public class Animal {
         private boolean hasFur;
 
         public Animal(String color, int numberOfPaws, boolean hasFur){
+                 this.color = color;
+            this.numberOfPaws = numberOfPaws;
+            this.hasFur = hasFur;
         }
   
         public String getDescription(String color, int numberOfPaws, boolean hasFur){
