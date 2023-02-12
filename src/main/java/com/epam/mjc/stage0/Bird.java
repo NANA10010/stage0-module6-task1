edb40db05b7d0e5f8e88e04e583852b6bce7e345
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-12 18:33:26 +0400

    Update Bird.java

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -6,7 +6,7 @@ public class Bird extends Animal{
          }
 
          @Override
-         public String getDescription(String color, int numberOfPaws, boolean hasFur){
+         public String getDescription(){
              String ans = "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
              return ans;
          }
